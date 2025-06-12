09abd2a5280d0b2477ffa3ae2aa3d7d80ff5bc52
longhp2504 <157264591+longhp2504@users.noreply.github.com>
2025-06-13 00:34:30 +0700

    update entity.Vehicle
    update image, type

--- BE/src/main/java/com/rft/rft_be/entity/Vehicle.java
+++ BE/src/main/java/com/rft/rft_be/entity/Vehicle.java
@@ -36,7 +36,7 @@ public class Vehicle {
     @Column(name = "license_plate", length = 20)
     private String licensePlate;
 
-    @Column(name = "vehicle_types", length = 50)
+    @Column(name = "vehicle_type", length = 50)
     private String vehicleTypes;
 
     @Lob
@@ -44,7 +44,7 @@ public class Vehicle {
     private String vehicleFeatures;
 
     @Lob
-    @Column(name = "vehicle_image")
+    @Column(name = "vehicle_images")
     private String VehicleImages;
 
     @Enumerated(EnumType.STRING)
