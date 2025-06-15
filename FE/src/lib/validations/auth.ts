773b9767babe067512f6c188eec7ba01f0c91692
Minh Nghia <nmnghia1702@gmail.com>
2025-06-15 21:52:05 +0700

    add UI OTP and changepassword

--- FE/src/lib/validations/auth.ts
+++ FE/src/lib/validations/auth.ts
@@ -47,3 +47,28 @@ export const forgotPasswordSchema = z.object({
     .regex(/^[0-9]+$/, "Số điện thoại chỉ được chứa ký tự số."),
   email: z.string().email("Email không hợp lệ.").min(5).max(50),
 });
+
+export const verifyOTPSchema = z.object({
+  otp: z
+    .string()
+    .min(6, "Mã OTP phải có 6 ký tự.")
+    .max(6, "Mã OTP phải có 6 ký tự.")
+    .regex(/^[0-9]+$/, "Mã OTP chỉ được chứa ký tự số."),
+});
+
+export const resetPasswordSchema = z
+  .object({
+    password: z
+      .string()
+      .min(6, "Mật khẩu phải có ít nhất 6 ký tự.")
+      .max(50, "Mật khẩu không được vượt quá 50 ký tự.")
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,50}$/,
+        "Mật khẩu phải có ít nhất một chữ hoa, một chữ thường và một số."
+      ),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Mật khẩu xác nhận không khớp.",
+    path: ["confirmPassword"],
+  });
