29ad79a427ca354eb4a699ba62c7a966338c77d5
Ha Xuan Hau <Hauvs789@gmail.com>
2025-07-16 11:50:39 +0700

    user extra fee rule detail

--- BE/src/main/java/com/rft/rft_be/dto/extraFeeRule/ExtraFeeRuleDTO.java
+++ BE/src/main/java/com/rft/rft_be/dto/extraFeeRule/ExtraFeeRuleDTO.java
@@ -1,6 +1,15 @@
 package com.rft.rft_be.dto.extraFeeRule;
 
-public class extraFeeRuleDTO {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ExtraFeeRuleDTO {
     Integer maxKmPerDay;
     Integer feePerExtraKm;
     Integer allowedHourLate;
