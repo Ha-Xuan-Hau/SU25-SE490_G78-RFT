53667b93584415993262b720225eeb8bcc8c6dee
Nguyễn Minh Nghĩa <nmnghia1702@gmail.com>
2025-07-21 14:12:31 +0700

    Update Rating

--- BE/src/main/java/com/rft/rft_be/util/ProfanityValidator.java
+++ BE/src/main/java/com/rft/rft_be/util/ProfanityValidator.java
@@ -4,7 +4,9 @@ import java.util.Set;
 
 public class ProfanityValidator {
     private static final Set<String> PROFANITY_WORDS = Set.of(
-            "má", "mẹ", "cc", "đmm", "cl", "lồn", "cặc", "địt", "bú", "chịch", "fuck"
+            "má", "mẹ", "bố", "cha", "ông nội", "bà nội", "con đĩ", "con chó", "con cặc", "thằng ngu",
+            "đmm", "đm", "vkl", "vl", "cl", "lồn", "cặc", "buồi", "địt", "bú", "chịch", "nứng", "dâm", "phê", "đú",
+            "đéo", "đụ", "đù", "cđmm", "cc", "shit", "dm", "vlcc", "fuck", "fucking", "bitch", "dcm", "vcl", "lol", "wtf"
     );
 
     public static boolean containsProfanity(String text) {
