6a8d04db11c338c6880577bab3d5e9f411a0d4ad
phamtiep28 <tieppt28@gmail.com>
2025-07-28 00:23:46 +0700

    fix type open & close time

--- BE/src/main/java/com/rft/rft_be/service/admin/AdminUserServiceImpl.java
+++ BE/src/main/java/com/rft/rft_be/service/admin/AdminUserServiceImpl.java
@@ -274,8 +274,8 @@ public class AdminUserServiceImpl implements AdminUserService {
                 .totalEarned(totalEarned.doubleValue())
                 .validLicenses(licenseNumbers)
                 .registeredVehicles(vehicleNames)
-                .openTime(openTimeStr)
-                .closeTime(closeTimeStr)
+                .openTime(user.getOpenTime())
+                .closeTime(user.getCloseTime())
                 .build();
     }
 } 
