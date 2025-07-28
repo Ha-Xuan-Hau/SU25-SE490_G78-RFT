3f42e655bde5448060195a985d879910f0ba3111
longhp2504 <157264591+longhp2504@users.noreply.github.com>
2025-07-28 12:53:24 +0700

    update getQuantityOfAvailableVehiclesByThumb quantity
    update getQuantityOfAvailableVehiclesByThumb quantity

--- BE/src/main/java/com/rft/rft_be/service/vehicle/VehicleServiceImpl.java
+++ BE/src/main/java/com/rft/rft_be/service/vehicle/VehicleServiceImpl.java
@@ -784,9 +784,7 @@ public class VehicleServiceImpl implements VehicleService {
                 .filter(v -> !busyVehicleIds.contains(v.getId()))
                 .toList();
 
-        int totalQuantity = availableVehicles.stream()
-                .mapToInt(v -> v.getNumberVehicle() != null ? v.getNumberVehicle() : 1)
-                .sum();
+        int totalQuantity = availableVehicles.size();
 
         return AvailableVehicleQuantityOnlyDTO.builder()
                 .quantity(totalQuantity)
