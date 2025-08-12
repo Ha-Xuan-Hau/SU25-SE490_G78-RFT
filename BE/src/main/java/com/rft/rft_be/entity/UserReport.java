b42353a265a25090e710112923eadb2bace10ccb
Ha Xuan Hau <Hauvs789@gmail.com>
2025-08-13 02:26:39 +0700

    add status to user_report

--- BE/src/main/java/com/rft/rft_be/entity/UserReport.java
+++ BE/src/main/java/com/rft/rft_be/entity/UserReport.java
@@ -2,18 +2,9 @@ package com.rft.rft_be.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -61,8 +52,15 @@ public class UserReport {
     @JoinColumn(name = "booking_id")
     private Booking booking;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private UserReport.Status status = Status.PENDING;
+
     @CreationTimestamp
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    public enum Status{
+        PENDING, REJECTED, APPROVED
+    }
 }
